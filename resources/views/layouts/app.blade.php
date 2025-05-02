fa40a27c00225f88e362b14ec81f0344f6aa1d86
michaelhutajulu <120302607+michaelhutajulu@users.noreply.github.com>
2025-05-02 17:02:35 +0700

    update logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bintang Serasi</title>
+    <link rel="icon" href="{{ asset('logo2.png') }}" type="image/x-icon">
 
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
